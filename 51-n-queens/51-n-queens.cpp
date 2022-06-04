f5872a09cc7d19fc8e3e0efaa1f89cea216a13a3
Sanjeevan Bapat <72686889+Mikerufy@users.noreply.github.com>
2022-06-04 08:18:26 +0530

    Time: 95 ms (8.88%), Space: 50.5 MB (5.44%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool help2(vector< string>c,int col,int row){
+    for(int i=0;i<row;i++){
+        if(c[i][col]=='Q'){
+            return false;
+        }
+    }
+    for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+        if(c[i][j]=='Q'){
+            return false;
+        }
+    }
+     for(int i=row-1,j=col+1;i>=0&&j<c[0].size();i--,j++){
+        if(c[i][j]=='Q'){
+            return false;
+        }
+    }
+    return true;
+}
+    
+void help(vector<string>c,vector<vector<string>>&s , int row) {
+    if(c.size()==0);
+  if(row==c.size()){
+    s.push_back(c);
+      return;
+  }
+    
+  for(int i=0;i<c[0].size();i++){
+      if(help2(c,i,row)){
+          c[row][i]='Q';
+          help(c,s ,row+1);
+          c[row][i]='.';
+      }
+  }
+}
+
+vector<vector<string>> solveNQueens(int n) {
+        string st="";
+        vector<vector<string>>v ;
+        for(int i=0;i<n;i++){
+            st+='.';
+        }
+        vector<string>s(n,st);
+    
+        help(s,v ,0);
+        return v;
+    }
+};
