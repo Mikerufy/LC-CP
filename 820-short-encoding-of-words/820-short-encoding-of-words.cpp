15a5469bb5a443a5ddb06ac6eb953612b703a058
Sanjeevan Bapat <72686889+Mikerufy@users.noreply.github.com>
2022-06-20 20:11:03 +0530

    Time: 340 ms (8.92%), Space: 36.4 MB (50.00%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -4,7 +4,7 @@ public:
         return a.size()>b.size();
     }
     int minimumLengthEncoding(vector<string>& w) {
-        sort(w.begin(),w.end(),compare);
+        // sort(w.begin(),w.end(),compare);
         int cnt = 0;
         map<string,int> m;
         for(int i=0;i<w.size();i++){
