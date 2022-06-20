f58b5149f506115a6ad5c19686d53b7da85de1c0
Sanjeevan Bapat <72686889+Mikerufy@users.noreply.github.com>
2022-06-20 20:10:27 +0530

    Time: 451 ms (6.94%), Space: 36.5 MB (50.00%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    static bool compare(string a,string b){
+        return a.size()>b.size();
+    }
+    int minimumLengthEncoding(vector<string>& w) {
+        sort(w.begin(),w.end(),compare);
+        int cnt = 0;
+        map<string,int> m;
+        for(int i=0;i<w.size();i++){
+            int n = w[i].size();
+            for(int j=1;j<n;j++){
+                string s = w[i].substr(j);
+                m[s]++;
+            }
+        }
+        
+        for(int i=0;i<w.size();i++){
+            if(m.find(w[i])!=m.end()){
+                continue;
+            }else{
+                cnt+= w[i].size()+1;
+                m[w[i]]++;
+            }
+        }
+        return cnt;
+    }
+};
