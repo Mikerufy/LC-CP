7bb41a6c72d963efc3a4f7293deea14829548e42
Sanjeevan Bapat <72686889+Mikerufy@users.noreply.github.com>
2022-07-15 11:32:45 +0530

    Time: 37 ms (33.12%), Space: 23.4 MB (44.41%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
+    int dp[100][100];
     int help(vector<vector<int>> &v,int i, int j){
         if(i<0 or j<0 or i>=v.size() or j>= v[0].size())return 0;
         if(v[i][j] == 0)return 0;
+        if(dp[i][j] != -1)return dp[i][j];
         int x[4] = {1,0,0,-1};
         int y[4] = {0,1,-1,0};
-        int ans = 1;
+        int ans = 0;
         v[i][j] = 0;
         for(int k=0;k<4;k++){
             int a = i + x[k];
             int b = j + y[k];
             if(a>=0 and b>=0 and a<v.size() and b< v[0].size() and v[a][b] == 1){
-                ans += help(v,a,b);
+                ans += 1 + help(v,a,b);
             }
         }
-        return ans;
+        return dp[i][j] = ans;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
+        memset(dp,-1,sizeof(dp));
         int m = grid.size();
         int n = grid[0].size();
-        int ans = 0;
+        int ans = -1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j])
                     ans = max(ans,help(grid,i,j));
             }
         }
-        return ans;
+        return ans+1;
     }
 };
